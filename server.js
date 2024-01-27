e96332fd5df9ed93242181916aa1ad6c7cc49116
Gregoire Dechaine <gregdechaine@me.com>
2024-01-27 20:10:16 +0000

    Login URL with Autodesk APS Working

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ const { PORT, SERVER_SESSION_SECRET } = require('./config.js');
 let app = express();
 app.use(express.static('wwwroot'));
 app.use(session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 }));
+app.use(require('./routes/auth.js'));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
